4014f0100f6b0011c0cec34e5bb7deda0005ce9a
Jasper <jw549@sussex.ac.uk>
2017-04-20 15:51:15 +0100

    Beginnings of a Simulator class starting to come together.

--- StockMarket/Main.java
+++ StockMarket/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO: Create main method to start the simulation.
+        // JUST USING THIS FOR TESTING AT THE MOMENT -- Jasper :)
+        new Simulator(0);
     }
 
 }
--- StockMarket/Simulator.java
+++ StockMarket/Simulator.java
@@ -1,5 +1,9 @@
 package StockMarket;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Simulator {
@@ -8,11 +12,54 @@ public class Simulator {
     HashMap<Trader, HashMap<String, Integer>> toBeBought; // Inner HashMap maps Company Name to number of shares sought for purchase.
     ArrayList<Trader> traders;
     ArrayList<Event> events;
+    ArrayList<Share> setupShares;
     int stockIndex; // in pence.
     String marketType; // Bull, Bear, Stable.
+    private static final int SIZE_DATA = 19;
 
-    public void initialiseData() {
-        // TODO: import data from initialData.xls and setup fields.
+    // In place of initialiseData and runSimulation.
+    public Simulator(int duration) {
+        calendar = new GregorianCalendar(2017, 0, 1);
+        traders = new ArrayList<>();
+        events = new ArrayList<>();
+        marketType = "Stable";
+
+        BufferedReader br = null;
+        String line = "";
+        try {
+            br = new BufferedReader(new FileReader("InitialDataV2.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                if(row.length == SIZE_DATA && !row[0].equals("")) { // This is a column of data.
+                    // TODO
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void run15Mins() {
+        // TODO: runs 1 15 minute period, i.e. traders trade, clock is updated, etc.
+    }
+
+    // excess will be negative when Supply > Demand.
+    private void changeSharePrice(String companyName, int excess) {
+        // TODO: increase if Demand > Supply, and vice versa.
+    }
+
+    private Event checkEvent() {
+        // TODO: return event if event is NOW, else null.
+        return null;
+    }
+
+    private void removeAllShares(String companyName) {
+        // TODO: removes shares from all portfolios with name matching companyName.
+    }
+
+    private int getSharePrice(int companyName) {
+        // TODO: returns share price of companyName.
+        return -1;
     }
 
 }
