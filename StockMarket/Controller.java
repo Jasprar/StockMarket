df14fb3b5433ed2b1d903acf6a64c4f33cca5397
Sheel Shah <shahsheel@outlook.com>
2017-04-21 13:46:40 +0100

    Instantiated Simulator

--- StockMarket/Controller.java
+++ StockMarket/Controller.java
@@ -2,6 +2,6 @@ package StockMarket;
 
 
 public class Controller {
-        Simulator sim = new Simulator(1); // dummy test need to get duration then instantiate(?) // need to overrwride it
+        Simulator sim = new Simulator(1); // dummy test need to get duration then instantiate(?) // need to overrride it
 
 }
