248fa8bac099c6f5b7bf21579b0b2eb97f9e7056
Jasper <jw549@sussex.ac.uk>
2017-04-21 11:44:13 +0100

    Adding some things to Portfolio that I needed for Simulator.

--- StockMarket/Porfolio.java
+++ StockMarket/Porfolio.java
@@ -1,4 +1,15 @@
 package StockMarket;
 
+import java.util.ArrayList;
+
 public class Porfolio {
+    private String clientName;
+    private int totalWorth;
+    ArrayList<Share> shares;
+
+    public Porfolio(String clientName) {
+        this.clientName = clientName;
+    }
+
+
 }
