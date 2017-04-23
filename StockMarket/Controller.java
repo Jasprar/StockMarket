90c6ff988c5795cf4203b13d37f84165e827e7cb
Jasper Weymouth <jw549@sussex.ac.uk>
2017-04-23 11:40:50 +0100

    Update Controller.java
    Added some comments on how I think set-up should work... Possibly no need for this class at all.

--- StockMarket/Controller.java
+++ StockMarket/Controller.java
@@ -3,5 +3,8 @@ package StockMarket;
 
 public class Controller {
         Simulator sim = new Simulator(); // dummy test need to get duration then instantiate(?) // need to overrride it
-
+// Essentially, what I think needs to happen is Main's main() method needs to run, it needs to create the GUI and the simulator
+// (Which no longer takes any input parameters). Then, when the user of the GUI hits run or start or whatever, a pop-up box appears stating
+// "How long (in minutes) would you like the simulation to run for?". Then main calls Simulator.runSimulation(int minutes) and off we go.
+// - Jasper.
 }
