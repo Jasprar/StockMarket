0e49e7f9ce05497719ec8315b1a815a737472628
jasprar <jw549@sussex.ac.uk>
2017-05-02 16:37:12 +0100

    created addToTotalWorth() method in Portfolio

--- StockMarket/Trader.java
+++ StockMarket/Trader.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 public abstract class Trader {
     protected ArrayList<Portfolio> portfolios;
-    private HashMap<Portfolio, ArrayList<Share>> sharesRemoved; // Used for returning shares to their respective portfolios if they don't sell.
     protected String event; // Contains the name of the company or commodity (or "Any") that the RandomTrader has to buy/sell during an event.
     protected ArrayList<ClientTracker> clientTrackers;
 
