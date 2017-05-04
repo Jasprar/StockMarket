dff252b09de549562c337f890912e671a99d798e
jasprar <jw549@sussex.ac.uk>
2017-05-04 15:01:11 +0100

    Refactored RandomTrader

--- StockMarket/Main.java
+++ StockMarket/Main.java
@@ -3,7 +3,7 @@ package StockMarket;
 public class Main {
 
     public static void main(String[] args) {
-        new Simulator().runSimulation();
+        new Simulator().runSimulation(1);
     }
 
 }
