43c155f56838b48b5784419f3b2cb328b04fb520
Sheel Shah <shahsheel@outlook.com>
2017-05-07 18:39:33 +0100

    Additions / more commenting

--- StockMarket/SimulatorTest.java
+++ StockMarket/SimulatorTest.java
@@ -1,12 +1,15 @@
 package StockMarket;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.omg.CORBA.portable.UnknownException;
 
+import javax.sound.sampled.Port;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,13 +36,13 @@ class SimulatorTest {
     @Test
     void getTime() {
         // At initialisation:
-        assertEquals(simulator.getTime(), "00:00:00");
+        assertEquals(simulator.getTime(), "09:00:00");
     }
 
     @Test
     void getDate() {
         // At initialisation:
-        assertEquals(simulator.getDate(), "01-01-2017");
+        assertEquals(simulator.getDate(), "02-01-2017");
     }
 
     @Test
@@ -61,7 +64,7 @@ class SimulatorTest {
     }
 
     @Test
-    void getWealth(){
+    void getWealth() {
         //Initialisation:
         try {
             ArrayList<Double> wealth = new ArrayList<>();
@@ -77,14 +80,14 @@ class SimulatorTest {
             wealth.add(3.3436761E7);
             assertEquals(simulator.getTotalWorth(), wealth); //Should not exist
             System.out.println(simulator.getTotalWorth());
-        }catch(IllegalArgumentException e){
-            throw(e);
+        } catch (IllegalArgumentException e) {
+            throw (e);
         }
 
     }
 
     @Test
-    void getIncorrectWealth(){
+    void getIncorrectWealth() {
         //Initialisation:
         try {
             ArrayList<Double> wealth = new ArrayList<>();
@@ -99,8 +102,8 @@ class SimulatorTest {
             wealth.add(3.1583543E7);
             wealth.add(3.3436761E7);
             assertNotSame(simulator.getTotalWorth(), wealth, "Wealth does not match - Correct"); //Should not exist
-        }catch(IllegalArgumentException e){
-            throw(e);
+        } catch (IllegalArgumentException e) {
+            throw (e);
         }
 
     }
@@ -114,56 +117,74 @@ class SimulatorTest {
         HashMap<String, Integer> sharePrices = new HashMap<>();
         BufferedReader br = new BufferedReader(new FileReader("InitialDataV2.csv"));
         String line;
-        while((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
             String[] row = line.split(",");
-            if(row.length == Simulator.SIZE_DATA && row[0].length() != 0) {
+            if (row.length == Simulator.SIZE_DATA && row[0].length() != 0) {
                 sharePrices.put(row[0], Integer.parseInt(row[3]));
             }
         }
-        while(companyNames.hasNext()) {
+        while (companyNames.hasNext()) {
             String companyName = companyNames.next();
-            assertEquals((int)simulator.getSharePrice(companyName), (int)sharePrices.get(companyName));
+            assertEquals((int) simulator.getSharePrice(companyName), (int) sharePrices.get(companyName));
             i++;
         }
     }
 
     @Test
-    void getTotalShares(){
+    void getTotalShares() {
         //Not being used - should run with anything
-        assertEquals(simulator.getShares(),8);
-        assertEquals(simulator.getShares(),"qq3");
+        assertEquals(simulator.getShares(), 8);
+        assertEquals(simulator.getShares(), "qq3");
     }
 
 
     @Test
-    void getTotalNetworth(){
+    void getTotalNetworth() {
         List<String> companyNames = new ArrayList<>();
-        Set<String> companyNames1 =  simulator.getCompanyNames();
+        Set<String> companyNames1 = simulator.getCompanyNames();
         companyNames.addAll(companyNames1);
 
-        for(String s: companyNames) {
+        for (String s : companyNames) {
             int x = simulator.getNetWorth(s);
-            assertEquals(simulator.getNetWorth(s),x);
+            assertEquals(simulator.getNetWorth(s), x);
 
         }
     }
 
     @Test
-    void getIncorrectTotalNetWorth(){
-            List<String> companyNames = new ArrayList<>();
-            Set<String> companyNames1 =  simulator.getCompanyNames();
-            companyNames.addAll(companyNames1);
+    void getIncorrectTotalNetWorth() {
+        List<String> companyNames = new ArrayList<>();
+        Set<String> companyNames1 = simulator.getCompanyNames();
+        companyNames.addAll(companyNames1);
 
-            for(String s: companyNames) {
-                int x = simulator.getNetWorth(s) + 1;
-                assertNotSame(simulator.getNetWorth(s),x,"Wealth does not match with false data ");
+        for (String s : companyNames) {
+            int x = simulator.getNetWorth(s) + 1;
+            assertNotSame(simulator.getNetWorth(s), x, "Wealth does not match with false data ");
 
-            }
+        }
 
     }
 
+    @Test
+    void clientLeavesSimulation() {
+        ArrayList clientNames = new ArrayList<String>();
+        clientNames.addAll(simulator.getClientNames());
+        for (Object s : clientNames) {
+            Portfolio p = new Portfolio((String) s.toString());
+            simulator.leaveSimulation((String) s.toString()); //set the sell all method to true in the clients portfolio
+
+            //Sell the clients shares ..assert true.
+        }
 
 
+    }
 
+    @Test
+    void getCashHolding(){
+        List<Double> cashHolding = new ArrayList<>();
+        cashHolding.addAll(simulator.getCashHolding());
 
+        assertSame(cashHolding,100000);
+
+    }
 }
