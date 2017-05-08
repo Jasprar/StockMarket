1a2a06f3edef3ba49517b434f94e3c11f03597ab
Sheel Shah <shahsheel@outlook.com>
2017-05-08 20:36:14 +0100

    Test memory management on stevens laptop

---
+++ StockMarket/EventTest.java
@@ -0,0 +1,36 @@
+package StockMarket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by shahs on 08/05/2017.
+ */
+class EventTest {
+    @Test
+    void getStartDateTime() {
+
+    }
+
+    @Test
+    void getEndDateTime() {
+
+    }
+
+    @Test
+    void isBuy() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getMessage() {
+
+    }
+
+}
