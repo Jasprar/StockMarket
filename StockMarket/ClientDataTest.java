f75135cbb4ff963dd49f8d16eaadddc2bbb29c99
Steven <stevenkhshum@gmail.com>
2017-05-08 21:18:45 +0100

    Memory management fixed on tablets

--- StockMarket/ClientDataTest.java
+++ StockMarket/ClientDataTest.java
@@ -2,6 +2,8 @@ package StockMarket;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -10,64 +12,62 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClientDataTest {
 
     ClientData clientData;
-    @Test
-    void getClient() {
-
-    }
 
     @Test
-    void setClient() {
-
+    void setClient() throws NoSuchFieldException, IllegalAccessException {
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setClient("TEST");
+        assertTrue(clientData.getClient().equals("TEST"));
     }
+
+
+    @Test
+    void getClient() throws NoSuchFieldException, IllegalAccessException {
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setClient("TEST");
+        assertTrue(clientData.getClient().equals("TEST"));
+    }
+
 
     @Test
     void getWealth() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setWealth("TEST");
+        assertTrue(clientData.getWealth().equals("TEST"));
     }
 
     @Test
     void setWealth() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setWealth("TEST");
+        assertTrue(clientData.getWealth().equals("TEST"));
     }
 
     @Test
     void getCashHolding() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setCashHolding("TEST");
+        assertTrue(clientData.getCashHolding().equals("TEST"));
     }
 
     @Test
     void setCashHolding() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setCashHolding("TEST");
+        assertTrue(clientData.getCashHolding().equals("TEST"));
     }
 
     @Test
     void getShares() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setShares("TEST");
+        assertTrue(clientData.getShares().equals("TEST"));
     }
 
     @Test
     void setShares() {
-
+        ClientData clientData = new ClientData("Test Client", "Test Wealth", "Test Cash Holding", "Test Shares");
+        clientData.setShares("TEST");
+        assertTrue(clientData.getShares().equals("TEST"));
     }
-
-    @Test
-    void clientProperty() {
-
-    }
-
-    @Test
-    void wealthProperty() {
-
-    }
-
-    @Test
-    void cashHoldingProperty() {
-
-    }
-
-    @Test
-    void sharesProperty() {
-
-    }
-
 }
